a5a295afb6669e1a21e61532125a0c5d35e32b7e
devinjdangelo <devinjdangelo@gmail.com>
2019-08-05 12:30:50 -0400

    added methods from trout to update board state

--- ReconBot.py
+++ ReconBot.py
@@ -3,25 +3,38 @@ from reconchess import *
 
 
 class ReconBot(Player):
+    def __init__(self):
+        self.board = None
+        self.color = None
+        self.my_piece_captured_square = None
+
     def handle_game_start(self, color: Color, board: chess.Board, opponent_name: str):
-        pass
+        self.board = board
+        self.color = color
 
     def handle_opponent_move_result(self, captured_my_piece: bool, capture_square: Optional[Square]):
-        pass
+        # if the opponent captured our piece, remove it from our board.
+        self.my_piece_captured_square = capture_square
+        if captured_my_piece:
+            self.board.remove_piece_at(capture_square)
 
     def choose_sense(self, sense_actions: List[Square], move_actions: List[chess.Move], seconds_left: float) -> \
             Optional[Square]:
         return random.choice(sense_actions)
 
     def handle_sense_result(self, sense_result: List[Tuple[Square, Optional[chess.Piece]]]):
-        pass
+        # add the pieces in the sense result to our board
+        for square, piece in sense_result:
+            self.board.set_piece_at(square, piece)
 
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
-        return random.choice(move_actions + [None])
+        return None
 
     def handle_move_result(self, requested_move: Optional[chess.Move], taken_move: Optional[chess.Move],
                            captured_opponent_piece: bool, capture_square: Optional[Square]):
-        pass
+        # if a move was executed, apply it to our board
+        if taken_move is not None:
+            self.board.push(taken_move)
 
     def handle_game_end(self, winner_color: Optional[Color], win_reason: Optional[WinReason],
                         game_history: GameHistory):
