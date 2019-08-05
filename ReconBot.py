3ccd96fde14527c4cef38eb3a8fe1a963e59e762
devinjdangelo <devinjdangelo@gmail.com>
2019-08-05 17:28:48 -0400

    added _print_obs and fixed obs tracking bugs

--- ReconBot.py
+++ ReconBot.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from reconchess import *
 
 PieceDict = {'P':0,'N':1,'B':2,'R':3,'Q':4,'K':5,
@@ -8,19 +9,19 @@ class ReconBot(Player):
     def __init__(self):
         self.board = None
         self.color = None
-        self.my_piece_captured_square = None
-        
+    
+    @staticmethod
     def _fen_to_obs(fen):
         obs = np.zeros(shape=(13,8,8),dtype=np.int32)
         row = 0
-        col = 0
+        col = 7
         for char in fen:
             if char.isalpha():
                 charint = PieceDict[char]
                 obs[charint,col,row] = 1
                 row += 1
             elif char=='/':
-                col += 1
+                col -= 1
                 row = 0
             elif char.isdigit():
                 row += int(char)
@@ -30,36 +31,56 @@ class ReconBot(Player):
     def _get_obs(self):
         self.obs = self._fen_to_obs(self.board.fen())
 
+    @staticmethod
     def _square_to_col_row(square: Square):
         return square//8,square%8
 
-    def _piece_idx_at_col_row(col,row):
-        return np.argmax(self.obs[:,col,row])
-        
+    def _piece_idx_at_col_row(self,col,row):
+        if np.max(self.obs[:12,col,row])>0:
+            return np.argmax(self.obs[:,col,row])
+        else:
+            return None
+
+    def _print_obs(self,phase):
+        print('\n'+phase)
+        printobs = np.zeros(shape=(8,8),dtype=object)
+        for col in range(8):
+            for row in range(8):
+                idx = self._piece_idx_at_col_row(col,row)
+                if idx is not None:
+                    key = next(key for key, value in PieceDict.items() if value == idx)
+                else:
+                    key = '.'
+                printobs[col,row] = key
+
+        print(np.flip(printobs,axis=0))        
 
     def handle_game_start(self, color: Color, board: chess.Board, opponent_name: str):
         self.board = board
         self.color = color
         self._get_obs()
+        self._print_obs('game start')
 
     def handle_opponent_move_result(self, captured_my_piece: bool, capture_square: Optional[Square]):
         #when the opponent moves, we no longer have certainty that any previously known pieces
         #have not moved. Here we remove any piece that could have taken any legal move from the
         #known game state observation.
 
-        legal_moves = [_square_to_col_row(move.from_square) for move in self.board.legal_moves]
-        legal_col,legal_row = list(zip(*legal_moves))
-
+        #legal_moves = [self._square_to_col_row(move.from_square) for move in self.board.legal_moves]
+        #legal_col,legal_row = list(zip(*legal_moves))
+        #print(legal_col,legal_row)
         if self.color:
-            self.obs[6:11,legal_col,legal_row] = 0
+            self.obs[6:12,:,:] = 0
         else:
-            self.obs[:6,legal_col,legal_row] = 0
-            
+            self.obs[:6,:,:] = 0
+
         if captured_my_piece:
             col,row = self._square_to_col_row(capture_square)
-            self.obs[:11,col,row] = 0
+            self.obs[:12,col,row] = 0
             self.obs[12,col,row] = 1
             self.board.remove_piece_at(capture_square)
+
+        self._print_obs('opponent move')
 
     def choose_sense(self, sense_actions: List[Square], move_actions: List[chess.Move], seconds_left: float) -> \
             Optional[Square]:
@@ -69,9 +90,16 @@ class ReconBot(Player):
         # add the pieces in the sense result to our board
         for square, piece in sense_result:
             self.board.set_piece_at(square, piece)
-            col,row = self._square_to_col_row(capture_square)
-            piece_idx = PieceDict[piece.symbol]
-            self.obs[piece_idx,col,row] = 1
+            col,row = self._square_to_col_row(square)
+            if piece is not None:
+                piece_idx = PieceDict[piece.symbol()]
+                self.obs[piece_idx,col,row] = 1
+            else:
+                self.obs[:,col,row] = 0
+
+           
+
+        self._print_obs('handle sense')
 
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
         action = random.choice(move_actions)
@@ -88,6 +116,7 @@ class ReconBot(Player):
             col,row = self._square_to_col_row(taken_move.to_square)
             self.obs[piece_idx,col,row] = 1
 
+        self._print_obs('handle move result')
 
     def handle_game_end(self, winner_color: Optional[Color], win_reason: Optional[WinReason],
                         game_history: GameHistory):
