14cab588a7ee4443dc6c89cca4c43562bf1216ce
devinjdangelo <devinjdangelo@gmail.com>
2019-08-05 13:57:43 -0400

    convert fen to intial obs format

--- ReconBot.py
+++ ReconBot.py
@@ -1,16 +1,40 @@
 import random
 from reconchess import *
 
+PieceDict = {'P':0,'N':1,'B':2,'R':3,'Q':4,'K':5,
+        'p':6,'n':7,'b':8,'r':9,'q':10,'k':11}
 
 class ReconBot(Player):
     def __init__(self):
         self.board = None
         self.color = None
         self.my_piece_captured_square = None
+        
+    def _fen_to_obs(fen):
+        obs = np.zeros(shape=(13,8,8),dtype=np.int32)
+        row = 0
+        col = 0
+        for char in fen:
+            if char.isalpha():
+                charint = PieceDict[char]
+                obs[charint,col,row] = 1
+                row += 1
+            elif char=='/':
+                col += 1
+                row = 0
+            elif char.isdigit():
+                row += int(char)
+            else:
+                return obs
+            
+    def _get_obs(self):
+        self.obs = self._fen_to_obs(self.board.fen())
+        
 
     def handle_game_start(self, color: Color, board: chess.Board, opponent_name: str):
         self.board = board
         self.color = color
+        self._get_obs()
 
     def handle_opponent_move_result(self, captured_my_piece: bool, capture_square: Optional[Square]):
         # if the opponent captured our piece, remove it from our board.
