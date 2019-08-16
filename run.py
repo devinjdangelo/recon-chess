faab617824bd41e7b9ae7ba68cc6d92bbdba7981
devinjdangelo <devinjdangelo@gmail.com>
2019-08-16 16:58:29 -0400

    update n_rounds based on number of mpi ranks

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from Trainer import ReconTrainer
+from mpi4py import MPI 
 
+comm = MPI.COMM_WORLD 
+rank = comm.rank
+workers  = comm.Get_size()
 
 model_path = './models/'
 load_model = False
@@ -14,7 +18,7 @@ net_stat_path = 'Network Stats 1.csv'
 
 trainer = ReconTrainer(model_path,load_model,opponent_initial_model_path,score,score_smoothing,game_stat_path,net_stat_path)
 
-n_rounds = 256
+n_rounds = 1024//workers
 n_moves = 64
 
 epochs = 3
